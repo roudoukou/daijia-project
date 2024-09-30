554a96f5008df8b0d6cd4bcacf348dea3808d304
肉豆蔻 <1023876294@qq.com>
2024-09-30 22:52:02 +0800

    feat:预估订单数据

--- daijia-parent/web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
+++ daijia-parent/web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
@@ -3,14 +3,14 @@ package com.atguigu.daijia.customer.controller;
 import com.atguigu.daijia.common.aop.GuiguLogin;
 import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.customer.service.OrderService;
+import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
+import com.atguigu.daijia.model.vo.customer.ExpectOrderVo;
 import com.atguigu.daijia.model.vo.order.CurrentOrderInfoVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Tag(name = "订单API接口管理")
@@ -29,5 +29,14 @@ public class OrderController {
         return Result.ok(currentOrderInfoVo);
     }
 
+    @Autowired
+    private OrderService orderService;
+
+    @Operation(summary = "预估订单数据")
+    @GuiguLogin
+    @PostMapping("/expectOrder")
+    public Result<ExpectOrderVo> expectOrder(@RequestBody ExpectOrderForm expectOrderForm) {
+        return Result.ok(orderService.expectOrder(expectOrderForm));
+    }
 }
 
--- mp-weixin/mp-weixin-customer/config/config.js
+++ mp-weixin/mp-weixin-customer/config/config.js
@@ -1,3 +1,4 @@
 "use strict";
-const QqMapkey = "PYOBZ-Y6ZRZ-HMZXP-ZTMES-TNAQ7-WZFYS";
+// const QqMapkey = "PYOBZ-Y6ZRZ-HMZXP-ZTMES-TNAQ7-WZFYS";
+const QqMapkey = "GE3BZ-B4QLQ-Y3G5V-4VYK2-BZZTS-7MBI4";
 exports.QqMapkey = QqMapkey;
