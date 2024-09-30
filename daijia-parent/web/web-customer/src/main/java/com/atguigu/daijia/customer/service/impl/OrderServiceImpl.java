adb275946818d2aeda7119c9285aaa296dc298dd
肉豆蔻 <1023876294@qq.com>
2024-10-01 00:41:46 +0800

    feat:乘客下单接口

--- daijia-parent/web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
+++ daijia-parent/web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
@@ -4,11 +4,14 @@ import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.customer.service.OrderService;
 import com.atguigu.daijia.map.client.MapFeignClient;
 import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
+import com.atguigu.daijia.model.form.customer.SubmitOrderForm;
 import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
+import com.atguigu.daijia.model.form.order.OrderInfoForm;
 import com.atguigu.daijia.model.form.rules.FeeRuleRequestForm;
 import com.atguigu.daijia.model.vo.customer.ExpectOrderVo;
 import com.atguigu.daijia.model.vo.map.DrivingLineVo;
 import com.atguigu.daijia.model.vo.rules.FeeRuleResponseVo;
+import com.atguigu.daijia.order.client.OrderInfoFeignClient;
 import com.atguigu.daijia.rules.client.FeeRuleFeignClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -27,6 +30,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private FeeRuleFeignClient feeRuleFeignClient;
+
+    @Autowired
+    private OrderInfoFeignClient orderInfoFeignClient;
 
     //预估订单数据
     @Override
@@ -50,5 +56,35 @@ public class OrderServiceImpl implements OrderService {
         expectOrderVo.setDrivingLineVo(drivingLineVo);
         expectOrderVo.setFeeRuleResponseVo(feeRuleResponseVo);
         return expectOrderVo;
+    }
+
+    //乘客下单
+    @Override
+    public Long submitOrder(SubmitOrderForm submitOrderForm) {
+        //1 重新计算驾驶线路
+        CalculateDrivingLineForm calculateDrivingLineForm = new CalculateDrivingLineForm();
+        BeanUtils.copyProperties(submitOrderForm,submitOrderForm);
+        Result<DrivingLineVo> drivingLineVoResult = mapFeignClient.calculateDrivingLine(calculateDrivingLineForm);
+        DrivingLineVo drivingLineVo = drivingLineVoResult.getData();
+
+        //2 重新订单费用
+        FeeRuleRequestForm calculateOrderFeeForm = new FeeRuleRequestForm();
+        calculateOrderFeeForm.setDistance(drivingLineVo.getDistance());
+        calculateOrderFeeForm.setStartTime(new Date());
+        calculateOrderFeeForm.setWaitMinute(0);
+        Result<FeeRuleResponseVo> feeRuleResponseVoResult = feeRuleFeignClient.calculateOrderFee(calculateOrderFeeForm);
+        FeeRuleResponseVo feeRuleResponseVo = feeRuleResponseVoResult.getData();
+
+        //封装数据
+        OrderInfoForm orderInfoForm = new OrderInfoForm();
+        BeanUtils.copyProperties(submitOrderForm,orderInfoForm);
+        orderInfoForm.setExpectDistance(drivingLineVo.getDistance());
+        orderInfoForm.setExpectAmount(feeRuleResponseVo.getTotalAmount());
+        Result<Long> orderInfoResult = orderInfoFeignClient.saveOrderInfo(orderInfoForm);
+        Long orderId = orderInfoResult.getData();
+
+        //TODO 查询附近可以接单司机
+
+        return orderId;
     }
 }
