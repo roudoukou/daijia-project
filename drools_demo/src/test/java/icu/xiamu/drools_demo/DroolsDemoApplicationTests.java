2c10f8c8cc66f82a0c8b1a46e4f0ec60d78d7723
肉豆蔻 <1023876294@qq.com>
2024-09-30 22:43:54 +0800

    feat:drools入门案例

---
+++ drools_demo/src/test/java/icu/xiamu/drools_demo/DroolsDemoApplicationTests.java
@@ -0,0 +1,38 @@
+package icu.xiamu.drools_demo;
+
+import icu.xiamu.drools_demo.bean.Order;
+import org.junit.jupiter.api.Test;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsDemoApplicationTests {
+
+
+    @Autowired
+    private KieContainer kieContainer;
+
+    @Test
+    public void test(){
+        //从Kie容器对象中获取会话对象
+        KieSession session = kieContainer.newKieSession();
+
+        //Fact对象，事实对象
+        Order order = new Order();
+        order.setAmout(300);
+
+        //将Order对象插入到工作内存中
+        session.insert(order);
+
+        //激活规则，由Drools框架自动进行规则匹配，如果规则匹配成功，则执行当前规则
+        session.fireAllRules();
+        //关闭会话
+        session.dispose();
+
+        System.out.println("订单金额：" + order.getAmout() +
+                "，添加积分：" + order.getScore());
+    }
+
+}
