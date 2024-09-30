cd3ad898bf673f54019d3b60cb9039583f0fa0de
肉豆蔻 <1023876294@qq.com>
2024-09-30 19:34:53 +0800

    feat:远程调用司机端人脸模型服务接口

--- daijia-parent/service-client/service-driver-client/src/main/java/com/atguigu/daijia/driver/client/DriverInfoFeignClient.java
+++ daijia-parent/service-client/service-driver-client/src/main/java/com/atguigu/daijia/driver/client/DriverInfoFeignClient.java
@@ -43,5 +43,13 @@ public interface DriverInfoFeignClient {
     @PostMapping("/driver/info/updateDriverAuthInfo")
     Result<Boolean> UpdateDriverAuthInfo(@RequestBody UpdateDriverAuthInfoForm updateDriverAuthInfoForm);
 
+    /**
+     * 创建司机人脸模型
+     * @param driverFaceModelForm
+     * @return
+     */
+    @PostMapping("/driver/info/creatDriverFaceModel")
+    Result<Boolean> creatDriverFaceModel(@RequestBody DriverFaceModelForm driverFaceModelForm);
+
 
 }
