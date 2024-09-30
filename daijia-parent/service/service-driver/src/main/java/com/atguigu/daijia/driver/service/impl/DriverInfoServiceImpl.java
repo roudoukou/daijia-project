e30541480bcae65fb4b79bed9610dd55ad6eb894
肉豆蔻 <1023876294@qq.com>
2024-09-30 19:33:32 +0800

    feat:司机人脸模型返回peiqi

--- daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
+++ daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
@@ -15,11 +15,16 @@ import com.atguigu.daijia.model.entity.driver.DriverAccount;
 import com.atguigu.daijia.model.entity.driver.DriverInfo;
 import com.atguigu.daijia.model.entity.driver.DriverLoginLog;
 import com.atguigu.daijia.model.entity.driver.DriverSet;
+import com.atguigu.daijia.model.form.driver.DriverFaceModelForm;
 import com.atguigu.daijia.model.form.driver.UpdateDriverAuthInfoForm;
 import com.atguigu.daijia.model.vo.driver.DriverAuthInfoVo;
 import com.atguigu.daijia.model.vo.driver.DriverLoginVo;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tencentcloudapi.common.AbstractModel;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.BeanUtils;
@@ -157,5 +162,59 @@ public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverI
         boolean update = this.updateById(driverInfo);
         return update;
     }
+
+    //创建司机人脸模型
+    @Override
+    public Boolean creatDriverFaceModel(DriverFaceModelForm driverFaceModelForm) {
+        //根据司机id获取司机信息
+        DriverInfo driverInfo =
+                driverInfoMapper.selectById(driverFaceModelForm.getDriverId());
+        // try{
+        //
+        //     // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+        //     // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
+        //     // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+        //     Credential cred = new Credential(tencentCloudProperties.getSecretId(),
+        //             tencentCloudProperties.getSecretKey());
+        //     // 实例化一个http选项，可选的，没有特殊需求可以跳过
+        //     HttpProfile httpProfile = new HttpProfile();
+        //     httpProfile.setEndpoint("iai.tencentcloudapi.com");
+        //     // 实例化一个client选项，可选的，没有特殊需求可以跳过
+        //     ClientProfile clientProfile = new ClientProfile();
+        //     clientProfile.setHttpProfile(httpProfile);
+        //     // 实例化要请求产品的client对象,clientProfile是可选的
+        //     IaiClient client = new IaiClient(cred, tencentCloudProperties.getRegion(),
+        //             clientProfile);
+        //     // 实例化一个请求对象,每个接口都会对应一个request对象
+        //     CreatePersonRequest req = new CreatePersonRequest();
+        //     //设置相关值
+        //     req.setGroupId(tencentCloudProperties.getPersionGroupId());
+        //     //基本信息
+        //     req.setPersonId(String.valueOf(driverInfo.getId()));
+        //     req.setGender(Long.parseLong(driverInfo.getGender()));
+        //     req.setQualityControl(4L);
+        //     req.setUniquePersonControl(4L);
+        //     req.setPersonName(driverInfo.getName());
+        //     req.setImage(driverFaceModelForm.getImageBase64());
+        //
+        //     // 返回的resp是一个CreatePersonResponse的实例，与请求对象对应
+        //     CreatePersonResponse resp = client.CreatePerson(req);
+        //     // 输出json格式的字符串回包
+        //     System.out.println(AbstractModel.toJsonString(resp));
+        //     String faceId = resp.getFaceId();
+        //     if(StringUtils.hasText(faceId)) {
+        //         driverInfo.setFaceModelId(faceId);
+        //         driverInfoMapper.updateById(driverInfo);
+        //     }
+        // } catch (TencentCloudSDKException e) {
+        //     e.printStackTrace();
+        //     return false;
+        // }
+
+        driverInfo.setFaceModelId("peiqi");
+        driverInfoMapper.updateById(driverInfo);
+        return true;
+    }
+
 
 }
