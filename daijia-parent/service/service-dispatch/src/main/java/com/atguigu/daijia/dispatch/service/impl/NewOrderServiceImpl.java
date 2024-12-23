268e7e45dbc71f83557ad2982302dead9a719fba
肉豆蔻 <1023876294@qq.com>
2024-12-23 23:23:13 +0800

    feat:导入xxl工程

--- daijia-parent/service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/service/impl/NewOrderServiceImpl.java
+++ daijia-parent/service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/service/impl/NewOrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.atguigu.daijia.dispatch.service.impl;
 
 import com.atguigu.daijia.dispatch.service.NewOrderService;
+import com.atguigu.daijia.model.vo.dispatch.NewOrderTaskVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Service;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class NewOrderServiceImpl implements NewOrderService {
 
+    @Override
+    public Long addAndStartTask(NewOrderTaskVo newOrderTaskVo) {
+        return 0L;
+    }
 }
